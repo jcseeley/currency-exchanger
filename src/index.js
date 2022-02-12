6d86cc5d6d85121179ec7402809cab7364d1ef7b
Jase Seeley <jcseeley31590@yahoo.com>
2022-02-11 20:43:03 -0800

    add UI.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import test from './assets/images/test.jpg';
+import Exchanger from './js/exchanger';
 
-Image.src = test;
-
-$;
+$('#exchange').click(function() {
+  let amount = $('#amount').val();
+  let currency = $('#currency').val();
+  let promise = Exchanger.getExchange(currency, amount);
+  promise.then(function(response) {
+    const result = JSON.parse(response);
+    $('#conversion').text(`Conversion rate: 1 USD = ${result.conversion_rate}` + currency);
+    $('#value').text("Conversion Result: " + amount + " USD = " `${result.conversion_result}` + " " + currency);
+  }, function(error) {
+    $('#error').text(`There was an error processing your request: ${error}`);
+  });
+});
